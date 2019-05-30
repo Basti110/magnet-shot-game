b30802053d1906625101b844ed4244955ada3691
Alexander Dielen <alexander.dielen@rwth-aachen.de>
2019-05-30 23:12:55 +0200

    check if collision mesh exists

--- src/mesh_node.cpp
+++ src/mesh_node.cpp
@@ -8,6 +8,7 @@
 #include <polymesh/formats/obj.hh>
 
 #include <glow/objects/VertexArray.hh>
+#include <fstream>
 
 
 MeshNode::MeshNode(const glm::mat4& transform, const std::string& filename, PhysicsManager* physics, short collisionGroup, short collisionMask, float mass) :
@@ -18,42 +19,45 @@ MeshNode::MeshNode(const glm::mat4& transform, const std::string& filename, Phys
 
     const size_t idx = filename.find_last_of(".");
     const std::string collisionFilename = filename.substr(0, idx) + "Collision.obj";
+    std::ifstream collisionFile(collisionFilename);
 
     // collision mesh
-    pm::Mesh polymesh;
-    pm::obj_reader<float> obj_reader(collisionFilename, polymesh);
-    auto pos = obj_reader.get_positions().map([](std::array<float, 4> const& t) {
-            return btVector3(t[0], t[1], t[2]);
-    });
-    btDefaultMotionState* motionState = new btDefaultMotionState(to_bullet(transform));
+    if (collisionFile.good()) {
+        pm::Mesh polymesh;
+        pm::obj_reader<float> obj_reader(collisionFilename, polymesh);
+        auto pos = obj_reader.get_positions().map([](std::array<float, 4> const& t) {
+                return btVector3(t[0], t[1], t[2]);
+        });
+        btDefaultMotionState* motionState = new btDefaultMotionState(to_bullet(transform));
 
-    if (mass > 0) {
-        btConvexHullShape* collisionShape = new btConvexHullShape();
-        for (auto v : polymesh.vertices()) {
-            collisionShape->addPoint(pos[v]);
-        }
-        btVector3 inertia;
-        collisionShape->calculateLocalInertia(mass, inertia);
-        auto btInfo = btRigidBody::btRigidBodyConstructionInfo(mass, motionState, collisionShape, inertia);
-        // btInfo.m_friction = info.friction;
-        // btInfo.m_restitution = info.restitution;
-        // btInfo.m_linearDamping = info.linearDamping;
-        // btInfo.m_angularDamping = info.angularDamping;
-        mRigidBody = new btRigidBody(btInfo);
-    }
-    else {
-        btTriangleMesh* triangleMesh = new btTriangleMesh();
-        for (auto f : polymesh.faces()) {
-            std::vector<btVector3> verts;
-            for (auto v : f.vertices()) {
-                verts.push_back(pos[v]);
+        if (mass > 0) {
+            btConvexHullShape* collisionShape = new btConvexHullShape();
+            for (auto v : polymesh.vertices()) {
+                collisionShape->addPoint(pos[v]);
             }
-            triangleMesh->addTriangle(verts[0], verts[1], verts[2]);
+            btVector3 inertia;
+            collisionShape->calculateLocalInertia(mass, inertia);
+            auto btInfo = btRigidBody::btRigidBodyConstructionInfo(mass, motionState, collisionShape, inertia);
+            // btInfo.m_friction = info.friction;
+            // btInfo.m_restitution = info.restitution;
+            // btInfo.m_linearDamping = info.linearDamping;
+            // btInfo.m_angularDamping = info.angularDamping;
+            mRigidBody = new btRigidBody(btInfo);
         }
-        btCollisionShape* collisionShape = new btBvhTriangleMeshShape(triangleMesh, true);
-        mRigidBody = new btRigidBody(mass, motionState, collisionShape);
+        else {
+            btTriangleMesh* triangleMesh = new btTriangleMesh();
+            for (auto f : polymesh.faces()) {
+                std::vector<btVector3> verts;
+                for (auto v : f.vertices()) {
+                    verts.push_back(pos[v]);
+                }
+                triangleMesh->addTriangle(verts[0], verts[1], verts[2]);
+            }
+            btCollisionShape* collisionShape = new btBvhTriangleMeshShape(triangleMesh, true);
+            mRigidBody = new btRigidBody(mass, motionState, collisionShape);
+        }
+        mWorld->addRigidBody(mRigidBody, collisionGroup, collisionMask);
     }
-    mWorld->addRigidBody(mRigidBody, collisionGroup, collisionMask);
 
     // visual mesh
     mVertexArray = load_mesh_from_obj(filename, false);
