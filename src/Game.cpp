511379769f1c393c51d43e5414286cdbc6ebc4a9
Alexander Dielen <alexander.dielen@rwth-aachen.de>
2019-06-29 17:53:24 +0200

    added the clock tower

--- src/Game.cpp
+++ src/Game.cpp
@@ -600,6 +600,20 @@ void Game::initLevel()
         root->addChild(turbine);
     }
 
+    // add clock tower
+    root->addChild(new MeshNode(
+        glm::translate(glm::mat4(1), glm::vec3(-35.0f, 0.0f, -31.0f)),
+        "../../data/meshes/ClockTower.ply",
+        mPhysics, GROUP_STATIC_OBJECTS, GROUP_DYNAMIC_OBJECTS, 0.0f
+    ));
+    BoxNode* box = new BoxNode(
+        glm::translate(glm::mat4(1), glm::vec3(-35.0f, 2.5f, -31.0f)),
+        glm::vec3(0.15f, 0.55f, 0.15f),
+        mPhysics, GROUP_STATIC_OBJECTS, GROUP_DYNAMIC_OBJECTS, 0.0f
+    );
+    box->setColor(red);
+    root->addChild(box);
+
     // add dispenser
     glm::mat4 dispenserTransform = glm::mat4(1);
     dispenserTransform = glm::translate(dispenserTransform, glm::vec3(12, 0, 5));
